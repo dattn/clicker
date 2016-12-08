1441d39d00d18a10033faf8406b79de7abf26779
Daniel Duton <daniel@duton.lu>
2016-12-08 15:35:24 +0100

    move crafting items into separate component

--- src/vue/crafting.vue
+++ src/vue/crafting.vue
@@ -8,22 +8,7 @@
             </ul>
         </div>
         <div>
-            <div class="card-block" v-for="item in items">
-                <h3 class="card-title float-xs-left">
-                    <img :src="item.icon" class="icon" /> {{ item.label }}
-                </h3>
-                <div class="float-xs-right item-info">
-                    <p class="requirements">
-                        <span v-if="item.requires.energy" class="requirement">
-                            <img src="src/icons/energy.svg" class="icon" /> x {{ item.requires.energy }}
-                        </span>
-                        <span v-for="(amount, type) in item.requires.resources" class="requirement">
-                            <img :src="icon(type)" class="icon" /> x {{ amount }}
-                        </span>
-                    </p>
-                    <button class="btn btn-primary" @click="craft(item.type)" :disabled="!canCraft(item.type)">Craft</button>
-                </div>
-            </div>
+            <crafting-item v-for="item in items" :item="item"></crafting-item>
         </div>
     </div>
 </template>
@@ -52,7 +37,8 @@
 
 <script>
     import * as text        from '../js/helpers/text';
-    import { fromCategory, craft, canCraft, isAvailable, item } from '../js/crafting';
+    import { fromCategory, isAvailable } from '../js/crafting';
+    import CraftingItem from './crafting/item.vue';
 
     export default {
 
@@ -63,6 +49,10 @@
                     'resource', 'energy', 'upgrade', 'research'
                 ]
             }
+        },
+
+        components: {
+            CraftingItem
         },
 
         computed: {
@@ -81,12 +71,6 @@
 
         methods: {
             ...text,
-            craft,
-            canCraft,
-
-            icon(type) {
-                return item(type).icon;
-            },
 
             navClasses(category) {
                 return {
---
+++ src/vue/crafting/item.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="component-crafting-item card-block">
+        <h3 class="card-title float-xs-left">
+            <img :src="item.icon" class="icon" /> {{ item.label }}
+        </h3>
+        <div class="float-xs-right item-info">
+            <p class="requirements">
+                <span v-if="item.requires.energy" class="requirement">
+                    <img src="src/icons/energy.svg" class="icon" /> x {{ item.requires.energy }}
+                </span>
+                <span v-for="(amount, type) in item.requires.resources" class="requirement">
+                    <img :src="icon(type)" class="icon" /> x {{ amount }}
+                </span>
+            </p>
+            <button class="btn btn-primary" @click="craft(item.type)" :disabled="!canCraft(item.type)">Craft</button>
+        </div>
+    </div>
+</template>
+
+<style>
+    .component-crafting-item .icon {
+        height: 2em;
+    }
+
+    .component-crafting-item .requirement {
+        display: inline-block;
+    }
+
+    .component-crafting-item .requirement:not(:first-child) {
+        margin-left: 1em;
+    }
+
+    .component-crafting-item:not(:first-child) {
+        border-top: 1px solid #ddd;
+    }
+
+    .component-crafting-item  .item-info {
+        text-align: right;
+    }
+</style>
+
+<script>
+    import { craft, canCraft, item } from '../../js/crafting';
+
+    export default {
+
+        props: [ 'item' ],
+
+        methods: {
+            craft,
+            canCraft,
+
+            icon(type) {
+                return item(type).icon;
+            }
+        }
+
+    }
+</script>
