e8a134d7a8195322aaaf3ddd2d202850e32ee19c
Daniel Duton <daniel@duton.lu>
2016-12-08 17:31:27 +0100

    split energy vues into new vue

--- src/vue/energy.vue
+++ src/vue/energy.vue
@@ -1,44 +1,18 @@
 <template>
     <ul class="list-group energy-items">
-        <li class="list-group-item" v-for="(count, type) in items">
-            <div class="energy">{{ energy(type) }}</div>
-            <div class="icon-container">
-                <div class="icon-wrapper" v-for="n in count">
-                    <img :src="item(type).icon" class="icon" />
-                </div>
-            </div>
-        </li>
+        <energy-item v-for="(count, type) in items" :type="type" :count="count"></energy-item>
     </ul>
 </template>
 
-<style>
-    .energy-items .energy {
-        float: right;
-        font-size: 2em;
-    }
-
-    .energy-items .icon-container {
-        padding: 0 0.5em 0.5em 0;
-        line-height: 0.5ex;
-        font-size: 3em;
-    }
-
-    .energy-items .icon-wrapper {
-        display: inline-block;
-        height: 0.5em;
-        width: 0.5em;
-        overflow: visible;
-    }
-
-    .energy-items .icon-wrapper .icon {
-        height: 1em;
-    }
-</style>
-
 <script>
-    import { item, fromCategory } from '../js/crafting';
+    import { fromCategory } from '../js/crafting';
+    import EnergyItem from './energy/item.vue';
 
     export default {
+        components: {
+            EnergyItem
+        },
+
         computed: {
             items() {
                 const items = fromCategory('energy');
@@ -47,18 +21,6 @@
                     if (this.$store.state.items[items[i].type]) {
                         energy[items[i].type] = this.$store.state.items[items[i].type];
                     }
-                }
-                return energy;
-            }
-        },
-
-        methods: {
-            item,
-
-            energy(type) {
-                const energy = Math.round(item(type).generate.energy * 100) / 100;
-                if (energy > 0) {
-                    return '+' + energy;
                 }
                 return energy;
             }
---
+++ src/vue/energy/item.vue
@@ -0,0 +1,56 @@
+<template>
+    <li class="component-energy-item list-group-item">
+        <div class="energy">{{ energy(type) }}</div>
+        <div class="icon-container">
+            <div class="icon-wrapper" v-for="n in count">
+                <img :src="item(type).icon" class="icon" />
+            </div>
+        </div>
+    </li>
+</template>
+
+<style>
+    .component-energy-item .energy {
+        float: right;
+        font-size: 2em;
+    }
+
+    .component-energy-item .icon-container {
+        padding: 0 0.5em 0.5em 0;
+        line-height: 0.5ex;
+        font-size: 3em;
+    }
+
+    .component-energy-item .icon-wrapper {
+        display: inline-block;
+        height: 0.5em;
+        width: 0.5em;
+        overflow: visible;
+    }
+
+    .component-energy-item .icon-wrapper .icon {
+        height: 1em;
+    }
+</style>
+
+<script>
+    import { item } from '../../js/crafting';
+
+    export default {
+
+        props: [ 'type', 'count' ],
+
+        methods: {
+            item,
+
+            energy(type) {
+                const energy = Math.round(item(type).generate.energy * 100) / 100;
+                if (energy > 0) {
+                    return '+' + energy;
+                }
+                return energy;
+            }
+        }
+
+    }
+</script>
