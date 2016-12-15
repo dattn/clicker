54d0efe9fa714f01f894ba68ef5c00769f404d6e
Daniel Duton <daniel@duton.lu>
2016-12-15 10:46:42 +0100

    hash data

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -4,10 +4,43 @@ import * as mutations from './mutations';
 import * as getters from './getters';
 import { gameTime } from '../utils';
 import deepMerge from 'deepmerge';
+import sha1 from 'simple-sha1';
 
 Vue.use(Vuex);
 
 const deepClone = obj => JSON.parse(JSON.stringify(obj));
+
+const load = () => {
+    const encodedData = localStorage.getItem('CLICKER');
+    if (!encodedData) {
+        return {};
+    }
+    var rawData;
+    try {
+        rawData = atob(encodedData);
+    } catch(e) {
+        return {};
+    }
+
+    const index   = rawData.lastIndexOf(':');
+    const rawJSON = rawData.substring(0, index);
+    const hash    = rawData.substring(index + 1);
+    if (sha1.sync(rawJSON) !== hash) {
+        return {};
+    }
+
+    try {
+        return JSON.parse(rawJSON);
+    } catch(e) {}
+    return {};
+}
+
+const save = () => {
+    const rawJSON = JSON.stringify(store.state);
+    const hash    = sha1.sync(rawJSON);
+    const encodedData = btoa(rawJSON + ':' + hash);
+    return localStorage.setItem('CLICKER', encodedData);
+}
 
 const baseItems = {
     items: {
@@ -25,7 +58,7 @@ const defaultState = {
     windForce: 0,
     robotsOn: true
 };
-const savedState = JSON.parse(localStorage.getItem('CLICKER') || '{}');
+const savedState = load();
 const state = deepMerge(defaultState, savedState, {
     arrayMerge: d => d
 });
@@ -40,7 +73,6 @@ const store = new Vuex.Store({
 
 export default store;
 
-const save = () => localStorage.setItem('CLICKER', JSON.stringify(store.state));
 // save every minute
 setInterval(save, 60000);
 // add on unload
