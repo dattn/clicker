b598465061e74f13b066b3bf7493526878b94339
Daniel Duton <daniel@duton.lu>
2016-12-19 09:25:17 +0100

    add earth

---
+++ src/vue/layout/earth.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="component-layout-earth">
+        <img src="layout/earth.svg" />
+    </div>
+</template>
+
+<style lang="sass">
+    .component-layout-earth {
+        position: relative;
+
+        img {
+            width: 100%;
+        }
+    }
+</style>
--- src/vue/page/score.vue
+++ src/vue/page/score.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
+    <div class="component-page-score">
         <router-link :to="{ name: 'clicker' }" tag="button" class="btn btn-primary">back</router-link>
-        <h1>Score</h1>
+        <div class="earth-container">
+            <earth :style="earthStyle"></earth>
+        </div>
         <table>
             <tr v-for="(item, index) in states">
                 <td>{{ index }}</td>
@@ -11,10 +13,39 @@
     </div>
 </template>
 
+<style lang="sass">
+    @import "../../../node_modules/bootstrap/scss/bootstrap.scss";
+
+    html, body {
+        height: 100%;
+    }
+
+    .component-page-score {
+        min-height: 100%;
+        background-color: #000000;
+
+        .earth-container {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            height: 100%;
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+    }
+</style>
+
 <script>
     import { joinStates, leaveStates, stateUpdate } from '../../js/server/client';
+    import Earth from '../layout/earth.vue';
 
     export default {
+        components: {
+            Earth
+        },
+
         data() {
             return {
                 states: {}
@@ -35,6 +66,14 @@
         beforeDestroy() {
             this.stateUpdate.off();
             leaveStates();
+        },
+
+        computed: {
+            earthStyle() {
+                return {
+                    width: '50%'
+                }
+            }
         }
     }
 </script>
